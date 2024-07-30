155902f46b037628c12e876a8452a3d0ff2042ca
Maksim Litvinov <litvinovmksm@gamil.com>
2024-07-30 14:55:29 +0300

    fix imports

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,12 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'hexlet_code/version'
-require_relative 'hexlet_code/text'
-require_relative 'hexlet_code/textarea'
-require_relative 'hexlet_code/form'
-require_relative 'hexlet_code/tag'
-require_relative 'hexlet_code/submit'
-require_relative 'hexlet_code/html_formatter'
 require 'active_support/core_ext/string/inflections'
 
 module HexletCode
@@ -15,13 +8,15 @@ module HexletCode
   autoload :Text, 'hexlet_code/text'
   autoload :Textarea, 'hexlet_code/textarea'
   autoload :Label, 'hexlet_code/label'
+  autoload :Submit, 'hexlet_code/submit'
   autoload :FormBuilder, 'hexlet_code/form_builder'
   autoload :HtmlFormatter, 'hexlet_code/html_formatter'
 
   def self.form_for(entity, opts = {})
     builder = FormBuilder.new(entity, opts)
     yield builder
-    HtmlFormatter.format builder.structure
+    form = builder.structure
+    HtmlFormatter.format form
   end
 
   class Error < StandardError; end
